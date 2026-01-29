589b6b1dd4161e8af0a6c71fa7bef9f8517552cf
Oliver Ballegaard Westphalen <144014891+Hvaalen@users.noreply.github.com>
2026-01-29 21:40:58 +0100

    Add files via upload

---
+++ ChairsLib/Chair.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ChairsLib
+{
+    public class Chair
+    {
+        private string _model;
+        private int _maxWeight;
+
+        public int Id { get; set; }
+
+        public string Model
+        {
+            get => _model;
+            set
+            {
+                ValidateModel(value); // Kalder validate metoden
+                _model = value;
+            }
+        }
+
+        public int MaxWeight
+        {
+            get => _maxWeight;
+            set
+            {
+                ValidateMaxWeight(value); 
+                _maxWeight = value;
+            }
+        }
+
+        public bool HasPillow { get; set; }
+
+        public Chair() { }
+
+        public Chair(int id, string model, int maxWeight, bool hasPillow)
+        {
+            Id = id;
+            Model = model;
+            MaxWeight = maxWeight; 
+            HasPillow = hasPillow;
+        }
+
+        public void ValidateModel(string model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentNullException(nameof(model), "Model cannot be null");
+            }
+            if (model.Length < 2)
+            {
+                throw new ArgumentException("Model text must be at least 2 characters long.");
+            }
+        }
+
+        public void ValidateMaxWeight(int maxWeight)
+        {
+            if (maxWeight < 50)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maxWeight), "MaxWeight must be at least 50");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Chair [Id={Id}, Model={Model}, MaxWeight={MaxWeight}, HasPillow={HasPillow}]";
+        }
+    }
+}
---
+++ ChairsLib/ChairRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChairsLib
+{
+    public class ChairRepository
+    {
+        private List<Chair> _chairs = new List<Chair>();
+        private int nextId = 1;
+        public ChairRepository() 
+        {
+            _chairs.Add(new Chair(nextId++, "The Egg", 100, true));
+            _chairs.Add(new Chair(nextId++, "Swan", 120, true));
+            _chairs.Add(new Chair(nextId++, "E27", 130, true));
+        }
+
+
+        public List<Chair> GetAll()
+        {
+            return new List<Chair>(_chairs);
+        }
+
+        public Chair? GetById(int id)
+        {
+            return _chairs.FirstOrDefault(c => c.Id == id);
+        }
+
+        public Chair Add(Chair chair)
+        {
+            chair.Id = nextId++;
+            _chairs.Add(chair);
+            return chair;
+        }
+
+        public Chair? Remove(int id)
+        {
+            Chair? chair = GetById(id);
+            if (chair == null)
+            {
+                return null;
+            }
+                _chairs.Remove(chair);
+            return chair;
+        }
+    }
+}
